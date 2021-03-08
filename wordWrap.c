3eeecd060f3064d49ed541a8872d423539a5fba5
kritikp <kritik.patel@rutgers.edu>
2021-03-08 17:57:05 -0500

    Version 2

--- wordWrap.c
+++ wordWrap.c
@@ -7,6 +7,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
 typedef struct {
     size_t length;
     size_t used;
@@ -51,48 +55,79 @@ int sb_append(strbuf_t *L, char item)
     return 0;
 }
 
-int wordWrap(int width, int fd, int fw){
+int wordWrap(int width, int fr, int fw){
     //the buf that will hold the lines that we read from file
-    char  *buf;
+    char *buf;
     buf = (char*)malloc(width*sizeof(char));
+    int lTrack = 0;
 
     size_t nBytesread;
-    size_t bytesRead = width;
     //start reading the file 
-    while((nBytesread = read(fd, buf, bytesRead))!= -1){
+    
+    int whileIteration = 0;
+    strbuf_t word;
+    sb_init(&word, width);
+
+    while((nBytesread = read(fr, buf, width)) != 0){
         //after buf is loaded with words start wrapping 
+        if(DEBUG) printf("While loop iteration: %d\n", whileIteration);
         for(int i = 0; i < width; i++){
-            strbuf_t word;
-            sb_init(word, width);
             size_t nByteswrite;
-            //keeps track of the 
-            int lTrack = 0;
             //when the word is a space it is either the begining or the end of the word 
-            if(isspace(buf[i]){
+            if(buf[i] == '\0' && buf[i - 1] == '\0')
+                break;
+            if(isspace(buf[i]) || buf[i] == '\0'){
                 //if the word list is empty that means it is start so it 
                 if(word.used != 1){
                     //get the len of the word to write
                     nByteswrite = word.used - 1;
-                    write(fw, " ", 1);
-                    lTrack++;
-                    //write the word to the file after end
-                    write(fw, word, nByteswrite);
-                    //free the word once eneded
-                    sb_destroy(&word);
+                    if(width - lTrack < nByteswrite){   //If word doesn't fit on current line
+                        write(fw, "\n", 1);
+                        write(fw, word.data, nByteswrite);
+                        lTrack = nByteswrite;
+                        if(DEBUG) printf("Step %d: Wrote to next line %s\n", i, word.data);
+                        sb_destroy(&word);
+                        sb_init(&word, width);
+                    }
+                    else{
+                        //write the word to the file after end
+                        if(lTrack != 0){
+                            write(fw, " ", 1);
+                            lTrack++;
+                        }
+                        write(fw, word.data, nByteswrite);
+                        lTrack += nByteswrite;
+                        if(DEBUG) printf("Step %d: Wrote to same line %s\n", i, word.data);
+                        //free the word once ended
+                        sb_destroy(&word);
+                        sb_init(&word, width);
+                    }
+                    
                 }
                 else
                 {
-                    //do nothing
+                    if(DEBUG) printf("Step %d: Useless space\n", i);
                 }
                 
             }
             else{
                 //means the word is in process of making, so keep adding it 
                 sb_append(&word, buf[i]);
-                lTrack++;
+                if(DEBUG) printf("Step %d: Appended %c\n", i, buf[i]);
             }
             
         }
+        whileIteration++;
     }
+    
+    free(buf);
+    return 1;
+
+}
+
+int main(int argc, char argv[]){
+    int fr = open("readTesting.txt", O_RDONLY);
+    int fw = open("writeTesting.txt", O_WRONLY);
+    wordWrap(10, fr, fw);
 
 }
